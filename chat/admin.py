192a75f321919c23bb7e40b359004626046aca57
Jinwoo Hong <hongj54@mcmaster.ca>
2024-01-13 18:20:44 -0500

    add chat history feature

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Chat
 
+class ChatAdmin(admin.ModelAdmin):
+	list_display = ['content', 'room']
 # Register your models here.
+admin.site.register(Chat, ChatAdmin)
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from react_templates.render import render_react
+from .models import Chat
+# from react_templates.render import render_react
 
 def hello_world_page(request):
 	pass
@@ -9,4 +10,5 @@ def chatHome(request):
 	return render(request, "chatHome.html")
 
 def room(request, room_name):
-	return render(request, "room.html", {"room_name": room_name})
+	chat_log = Chat.objects.filter(room=room_name).order_by('-timestamp')[:20]
+	return render(request, "room.html", {"room_name": room_name, "chat_log": chat_log})
