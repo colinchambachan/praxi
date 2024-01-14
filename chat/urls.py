1536e4f4063fd56162e12d83aafdb33d9a541041
Jinwoo Hong <hongj54@mcmaster.ca>
2024-01-13 21:59:11 -0500

    final merge

--- chat/urls.py
+++ chat/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 	path("", views.chatHome, name='chatHome'),
 	path("<str:room_name>/", views.room, name='room'),
-	path("ai", views.aiChat, name="aiChat")
-
+	path("ai", views.aiChat, name="aiChat"),
+	path("User/aiChat/<str:lang>/<str:diff>", views.aiRoom, name='aiChat')
 ]
